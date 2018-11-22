43c0d56e10c315a40343afb4897b9115d4211c4d
krishnagotri <davoodece2013@gmail.com>
2018-11-22 23:56:39 +0530

    commit or new feature

--- file1.java
+++ file1.java
@@ -3,4 +3,7 @@ This is my first code from file1.java for amazon
 This is or new commit
 
 
-This is for more than one alias 
+This is for more than one alias
+
+
+This is the code or new application in newfeature branch 
